dc8da5538a1507051d0dc87bb6243661fb21e83b
Smitty0078 <smitty0078@gmail.com>
2021-10-04 17:46:40 -0400

    added routing

--- tequila_project/tequila-spa/src/app/app.module.ts
+++ tequila_project/tequila-spa/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const routes:Routes = [{path: '', component: HomeComponent }];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
